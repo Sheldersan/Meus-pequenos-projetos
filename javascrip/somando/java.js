56e2bfcb6bb3250092f325f7f3b3a502d713e2d8
Sheldersan <121521153+Sheldersan@users.noreply.github.com>
2023-03-21 22:34:18 -0300

    script

--- javascrip/somando/java.js
+++ javascrip/somando/java.js
@@ -1,16 +1,29 @@
-function somar() {
+function multiplicar() {
     let n1 = parseFloat( document.getElementById("n1").value);
     let n2 = parseFloat( document.getElementById("n2").value);
-    let resultado = n1 / n2;
+    let resultado = n1 + n2;
     document.getElementById("resultado").innerHTML = "O resultado da soma é: " + resultado;
     
 }
 
-function multiplicar() {
-    let n3 = parseFloat( documnet.getElementById("n3").value);
+function somar() {
+    let n3 = parseFloat( document.getElementById("n3").value);
     let n4 = parseFloat( document.getElementById("n4").value);
     let res = n3 * n4;
-    documnet.getElementById("res").innerHTML =  res;
+    document.getElementById("res").innerHTML = res;
 }
+function verificaridade(){
+    var idade = parseFloat(document.getElementById("idade").value)
+
+    if (idade >=18) {
+        document.getElementById('res-idade').innerHTML = 'Você é maior de idade.' ;
+    } else {
+        document.getElementById('res-idade').innerHTML = 'Você é menor de idade.'
+    }
+}
+
+
+
+
 
 
